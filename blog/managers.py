67bac193eb05919d66dbd244596195dad7501600
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-10-04 23:28:52 +0800

    Add manager for Posts

---
+++ blog/managers.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class PostManager(models.Manager):
+
+    def get_queryset(self):
+        return super(PostManager, self).get_queryset()
+
+    def get_recent_posts(self):
+        queryset = self.get_queryset().filter(
+            kind='post').filter(status='public').order_by('-created_at')[:5]
+
+        return queryset
+
+    def get_pinned_posts(self):
+        queryset = self.get_queryset().filter(kind='pinned_post'
+                                              ).filter(status='public')
+
+        return queryset
