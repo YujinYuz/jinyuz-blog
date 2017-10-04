e0b86bb536d8287d03c24304d4bfa6a722985bdb
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-10-04 23:33:34 +0800

    Add more fields to Post

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from .managers import PostManager
 
 
 SOCIAL_IMAGE = (
@@ -8,16 +10,32 @@ SOCIAL_IMAGE = (
     ('linkedin', 'LinkedIn'),
 )
 
+POST_KIND = (
+    ('post', 'Post'),
+    ('pinned_post', 'Pinned Post'),
+    ('draft', 'Draft'),
+)
+
+
+POST_STATUS = (
+    ('public', 'Public'),
+    ('private', 'Private'),
+)
+
 
 class Post(models.Model):
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    kind = models.CharField(max_length=100, choices=POST_KIND, default='draft')
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    body = models.TextField()
-    tags = models.CharField(max_length=255)
+    body = RichTextField()
+    tags = models.CharField(max_length=255, blank=True)
+    status = models.CharField(
+        max_length=100, choices=POST_STATUS, default='public')
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = PostManager()
 
     def __str__(self):
         return "{title}".format(title=self.title)
