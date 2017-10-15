2175e046b8d689fe75605dd937f6902358b48e9b
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-10-15 19:04:11 +0800

    Use get for retrieving environment variable.

--- blogsite/settings/production.py
+++ blogsite/settings/production.py
@@ -4,7 +4,7 @@ from .base import *
 
 DEBUG = False
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-SECRET_KEY = os.environ('SECRET_KEY', 'asdfasdf')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'asdfasdf')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
