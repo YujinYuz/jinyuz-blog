4cfea0699bc6d17f785669eca43a4c55cf3a0a82
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-10-15 18:36:38 +0800

    Add SECRET KEY for production.

--- blogsite/settings/production.py
+++ blogsite/settings/production.py
@@ -4,6 +4,7 @@ from dj_database_url import config
 
 DEBUG = False
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+SECRET_KEY = config('SECRET_KEY')
 
 DATABASES = {
     'default': dj_database_url.config(
