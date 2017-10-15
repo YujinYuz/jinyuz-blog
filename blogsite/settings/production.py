7cb9ed0053c2b974334fd1f93a7bfd0c29da3421
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-10-15 19:53:32 +0800

    Add additional settings for production.

--- blogsite/settings/production.py
+++ blogsite/settings/production.py
@@ -9,3 +9,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'asdfasdf')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'dev.jinyuz@gmail.com'
+EMAIL_HOST_PASSWORD = '123523qwe'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+SERVER_EMAIL = EMAIL_HOST_USER
