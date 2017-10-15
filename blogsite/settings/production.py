12c8afa3506ba6c621d743396d784a43157bb5cd
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-10-15 18:32:16 +0800

    Modify settings for production.

--- blogsite/settings/production.py
+++ blogsite/settings/production.py
@@ -1,4 +1,12 @@
 from .base import *
+import dj_database_url
+from dj_database_url import config
 
 DEBUG = False
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config
+    )
+}
