63479adc68dc088c530f41a776691107841b2c7f
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-10-15 20:20:57 +0800

    Try to add WHITENOISE_ROOT and see if it helps.

--- blogsite/settings/production.py
+++ blogsite/settings/production.py
@@ -4,7 +4,8 @@ from .base import *
 
 DEBUG = os.environ.get('DJANGO_DEBUG', False) == 'True'
 ALLOWED_HOSTS = ['*', 'jinyuz-blog.herokuapp.com']
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+WHITENOISE_ROOT = os.path.join(PROJECT_ROOT_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 SECRET_KEY = os.environ.get('SECRET_KEY', 'asdfasdf')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
