5ea531cbcce6aa0fa2bd4285c73f4ef40ca42b8f
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-10-15 21:03:01 +0800

    Use whitenoise staticfile compression.

--- blogsite/settings/production.py
+++ blogsite/settings/production.py
@@ -4,8 +4,7 @@ from .base import *
 
 DEBUG = os.environ.get('DJANGO_DEBUG', False) == 'True'
 ALLOWED_HOSTS = ['*', 'jinyuz-blog.herokuapp.com']
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECRET_KEY = os.environ.get('SECRET_KEY', 'asdfasdf')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
