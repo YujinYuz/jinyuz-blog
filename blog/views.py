a78782ae22d0da82ae3d67e08b8b773c5c86c3c1
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-10-18 19:30:33 +0800

    Use post_set instead of querying posts of a category.

--- blog/views.py
+++ blog/views.py
@@ -9,17 +9,11 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Post.objects.get_recent_posts()
 
-
 class DetailView(generic.DetailView):
-    model = Post
     template_name = 'blog/post_detail.html'
+    model = Post
 
 
 class CategoryView(generic.DetailView):
     template_name = 'blog/category_list.html'
     model = Category
-
-    def get_context_data(self, **kwargs):
-        context = super(CategoryView, self).get_context_data(**kwargs)
-        context['category_posts'] = Post.objects.filter(category=context['category'])
-        return context
