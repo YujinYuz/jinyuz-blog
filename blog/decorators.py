cff95d0f23bbf0ac387c6d5a9f99b4e8252e030f
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-11-07 23:22:02 +0800

    Use get_object_or_404 to prevent internal server error.

--- blog/decorators.py
+++ blog/decorators.py
@@ -1,9 +1,10 @@
+from django.shortcuts import get_object_or_404
 from django.core.exceptions import PermissionDenied
 from blog.models import Post
 
 def public_post_only(function):
     def wrap(request, *args, **kwargs):
-        post = Post.objects.get(slug=kwargs['slug'])
+        post = get_object_or_404(Post, slug=kwargs['slug'])
         if post.status == 'public':
             return function(request, *args, **kwargs)
         else:
