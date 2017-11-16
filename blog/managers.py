d898e13c6bec46b617687f788ebd113919aa0f06
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-11-17 00:11:21 +0800

    Fix about page nothing is displayed.

--- blog/managers.py
+++ blog/managers.py
@@ -16,8 +16,9 @@ class PostManager(models.Manager):
         queryset = self.get_queryset().get(kind='intro', status='public')
         return queryset
 
-    def get_pinned_posts(self):
+    def get_pinned_post(self):
         queryset = self.get_queryset().get(kind='pinned_post', status='public')
+        return queryset
 
     def get_all_posts(self):
         queryset = self.get_queryset().filter(status='public')
--- blogsite/views.py
+++ blogsite/views.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.views import generic
+from django.shortcuts import get_object_or_404
 from blog.models import Post
 from jyz.utils import github
 
-class IndexView(generic.ListView):
+class IndexView(generic.TemplateView):
     template_name = 'blogsite/index.html'
-    context_object_name = 'latest_blog_posts'
-
-    def get_queryset(self):
-        return Post.objects.get_recent_posts()
 
     def get_context_data(self, *args, **kwargs):
         context = super(IndexView, self).get_context_data(*args, **kwargs)
@@ -23,8 +20,7 @@ class AboutView(generic.TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(AboutView, self).get_context_data(*args, **kwargs)
         context['repos'] = github.load_user_repositories(username='yujinyuz', forked=False)
-        context['post'] = Post.objects.get_pinned_posts()
-
+        context['post'] = Post.objects.get_pinned_post()
         return context
 
 class ContactView(generic.TemplateView):
