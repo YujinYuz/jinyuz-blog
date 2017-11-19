5ddf227dadb80e8a737ffa9bc7fe823b51371372
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-11-19 21:48:43 +0800

    Fix slug patterns. Just use \w-

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^(?P<slug>[a-zA-Z0-9-]+)/$', DetailView.as_view(), name='detail'),
+    url(r'^(?P<slug>[\w-]+)/$', DetailView.as_view(), name='detail'),
     url(r'^category/(?P<slug>[a-zA-Z0-9-]+)/$',
         CategoryView.as_view(), name='category'),
 ]
